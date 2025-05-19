7df936889595b255e29fa7ac720b9229dcfa56ea
lai2-24 <lai2-24@itismeucci.com>
2025-05-19 12:08:42 +0200

    risolti probemi index php e aggiunta funzione async

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,11 @@ import { useState } from "react";
 function App() {
   const [alunni, setAlunni] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [inserimento, setInserimento]=useState(false);
+  const [nome, setNome]=useState("");
+  const [cognome, setCognome]=useState("");
 
+  //asincrona
   function loadA(){
     setLoading(true);
     fetch('http://localhost:8080/alunni')
@@ -13,6 +17,33 @@ function App() {
       setAlunni(data)
       setLoading(false);
     });
+  }
+
+  //non asincrona (potrebbe avere dei delay)
+  async function loadB(){
+    setLoading(true);
+    const response = await fetch('http://localhost:8080/alunni');
+    const data = response.json();
+      setAlunni(data);
+      setLoading(false);
+  
+  }
+
+  function salvaAlunno(){
+    setInserimento(false);
+    fetch('http://localhost:8080/alunni', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({nome: nome, cognome: cognome})
+    })
+    .then(response => response.json())
+    .then(data => loadA());
+  }
+
+  function showInserimento(){
+    setInserimento(!inserimento);
   }
 
   return (
@@ -28,13 +59,22 @@ function App() {
           )
         }
       </table>
-       {loading &&
+      {loading &&
         <p>Caricamento... Attendere</p>
-       }
-       {alunni.length === 0 && !loading &&
-        <button onClick={loadA}>
-          Carica Alunni
-        </button>
+      }
+      {alunni.length === 0 && !loading &&
+        <button onClick={loadA}>Carica Alunni</button>
+      }
+      {alunni.length>0 && !inserimento &&
+        <button onClick={showInserimento}>inserisci</button>
+      }
+      {inserimento && 
+        <div>
+          <input type='text' name='cognome' placeholder='Nome' onChange={e => setNome(e.target.value)}></input><br></br>
+          <input type='text' name='cognome' placeholder='Cognome' onChange={e => setCognome(e.target.value)}></input><br></br>
+          <button onClick={salvaAlunno}>salva</button>
+          <button onClick={showInserimento}>annulla</button>
+        </div>
       }
     </>
   );
--- php/index.php
+++ php/index.php
@@ -7,6 +7,21 @@ require __DIR__ . '/includes/Db.php';
 
 $app = AppFactory::create();
 
+//---------------Alunni---------------------
+
+// curl http://localhost:8080/alunni
 $app->get('/alunni', "AlunniController:index");
+
+// curl http://localhost:8080/alunni/2
+$app->get('/alunni/{id}', "AlunniController:show");
+
+// curl -X POST http://localhost:8080/alunni -H "Content-Type: application/json" -d '{"sezione" : "4C" , "anno": "2020"}';
+$app->post('/alunni', "AlunniController:create");
+
+// curl -X PUT http://localhost:8080/alunni/2 -H "Content-Type: application/json" -d '{"sezione" : "3A" , "anno": "2024"}';
+$app->put('/alunni/{id}', "AlunniController:update");
+
+// curl -X DELETE http://localhost:8080/alunni/2
+$app->delete('/alunni/{id}', "AlunniController:destroy");
 
 $app->run();
